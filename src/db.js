112f06d8fd573729b1e8e9d9d809b8ff1c5cb52f
Soltan <mahmudovsoltan477@gmail.com>
2025-10-25 23:30:28 +0400

    Refactor code structure for improved readability and maintainability

--- src/db.js
+++ src/db.js
@@ -1,61 +1,103 @@
-  import image1 from './assets/images/software.png'
-  import image2 from './assets/images/paperGame.png'
-  import image3 from './assets/images/health.png'
-  import image4 from './assets/images/consultan.png'
-  import image5 from './assets/images/Screenshot 2025-02-16 044445.png'
-  import image6 from './assets/images/Screenshot 2025-06-10 144157.png'
-  
-  
-  
-  
- export const data = [
-    {
-        "name":"goldenvitpharma",
-        "description":"GoldenVit Pharma – This project was developed to allow users to browse and order pharmaceutical products online. The website features a responsive design and provides a smooth user experience. A product catalog, search functionality, and ordering system have been successfully integrated.",
-        "image":image6,
-        "tecnolgy":["React","React-router-dom","Redux-Toolkit","Responsive,'Swiper'"],
-        "link":"https://goldenvitpharma.com/"
-    },
-    // {
-    //     "name":"E-Commers Website",
-    //     "description":"This project demonstrates my ability to work with APIs and create dynamic, data-driven web applications. Built using React, it fetches and displays data from an external API, showcasing my skills in handling asynchronous operations, state management, and responsive design.",
-    //     "image":image5,
-    //     "tecnolgy":["React","React-router-dom","React Bootstrap","Redux-Toolkit","RTK Query","Responsive"],
-    //     "link":"https://react-api-gold-two.vercel.app/"
-    // },
-    {
-        "name":"Helath-coaching",
-        "description":"This project is a fully responsive and user-friendly website designed for a health coaching service. It provides visitors with essential information about health coaching programs, nutrition tips, and wellness strategies. The website features a clean and modern design, easy navigation, and interactive elements to engage users.",
-        "image":image3,
-        "tecnolgy":["Html","Tailwind CSS","React","React-router-dom","Redux-Toolkit","Swiper","Responsive"],
-        "link":"https://helath-coaching.netlify.app/"
-    },
-    {
-        "name":"Consulting",
-        "description":"This project is a professional and responsive website designed for a consulting service. It provides visitors with comprehensive information about consulting services, client success stories, and contact details. The website features a modern and clean design, easy navigation, and interactive elements to engage users.",
-        "image":image4,
-        "tecnolgy":["Html","Css","React","React-router-dom","Redux-Toolkit","Swiper"],
-        "link":"https://consalting.netlify.app/"
-    }
-    
-    
-    // ,
-    // {
-    //     "name":"Software Village",
-    //     "description":"   This project was developed for Software Village to help students learn Frontend, Backend, Mobile Development, and Design. The course provides hands-on experience with modern technologies, real-world projects, and interactive lessons, preparing students for the industry.",
-    //     "image":image1,
-    //     "tecnolgy":["Html","Css","React","React-router-dom"],
-    //     "link":"https://ilklayihe.netlify.app/"
-    // },
-    // {
-    //     "name":"Rock-Paper-Scissors!",
-    //     "description":"This project is a digital version of the classic Rock-Paper-Scissors game, designed to provide a fun and interactive experience for users. It allows players to challenge the computer in a game of strategy and luck, with instant results and a score tracker to keep the competition exciting",
-    //     "image":image2,
-    //     "tecnolgy":["Html","Css","Javascript"],
-    //     "link":"https://homwork-game.netlify.app/"
-    // },
-  
-    
-   
-]
+import image3 from "./assets/images/health.png";
+import image4 from "./assets/images/consultan.png";
+import image6 from "./assets/images/Screenshot 2025-06-10 144157.png";
+import image1 from "./assets/images/FIlmalisa.png";
+import image2 from "./assets/images/Tik_tak.png";
 
+export const data = [
+  {
+    name: "goldenvitpharma",
+    description:
+      "GoldenVit Pharma – This project was developed to allow users to browse and order pharmaceutical products online. The website features a responsive design and provides a smooth user experience. A product catalog, search functionality, and ordering system have been successfully integrated.",
+    image: image6,
+    tecnolgy: [
+      "React",
+      "React-router-dom",
+      "Redux-Toolkit",
+      "Responsive,'Swiper'",
+    ],
+    link: "https://goldenvitpharma.com/",
+  },
+  // {
+  //     "name":"E-Commers Website",
+  //     "description":"This project demonstrates my ability to work with APIs and create dynamic, data-driven web applications. Built using React, it fetches and displays data from an external API, showcasing my skills in handling asynchronous operations, state management, and responsive design.",
+  //     "image":image5,
+  //     "tecnolgy":["React","React-router-dom","React Bootstrap","Redux-Toolkit","RTK Query","Responsive"],
+  //     "link":"https://react-api-gold-two.vercel.app/"
+  // },
+  {
+    name: "Helath-coaching",
+    description:
+      "This project is a fully responsive and user-friendly website designed for a health coaching service. It provides visitors with essential information about health coaching programs, nutrition tips, and wellness strategies. The website features a clean and modern design, easy navigation, and interactive elements to engage users.",
+    image: image3,
+    tecnolgy: [
+      "Html",
+      "Tailwind CSS",
+      "React",
+      "React-router-dom",
+      "Redux-Toolkit",
+      "Swiper",
+      "Responsive",
+    ],
+    link: "https://helath-coaching.netlify.app/",
+  },
+  {
+    name: "Consulting",
+    description:
+      "This project is a professional and responsive website designed for a consulting service. It provides visitors with comprehensive information about consulting services, client success stories, and contact details. The website features a modern and clean design, easy navigation, and interactive elements to engage users.",
+    image: image4,
+    tecnolgy: [
+      "Html",
+      "Css",
+      "React",
+      "React-router-dom",
+      "Redux-Toolkit",
+      "Swiper",
+    ],
+    link: "https://consalting.netlify.app/",
+  },
+  {
+    name: "Filmalisa",
+    description:
+      "CineMatch is a dynamic and user-friendly movie discovery web application built with a modern front-end stack. The platform allows users to create a personal account, browse an extensive collection of films, and manage their cinematic experience.This project is a professional and responsive website designed for a consulting service. It provides visitors with comprehensive information about consulting services, client success stories, and contact details. The website features a modern and clean design, easy navigation, and interactive elements to engage users.",
+    image: image1,
+    tecnolgy: [
+      "Html",
+      "Css",
+      "Javascript",
+      "Swiper",
+    ],
+    link: "https://mahmudovsoltan.github.io/Filmalisa/",
+  },
+  {
+    name: "Tik Tak",
+    description:
+      "Tik Tak is a feature-rich e-commerce web application designed to provide a seamless and intuitive online shopping experience. The platform empowers users to browse, search, and manage their potential purchases with ease.",
+    image: image2,
+    tecnolgy: [
+      "Html",
+      "Css",
+      "Next.js",
+      "TypeScript",
+      "Redux-Toolkit",
+      "Swiper",
+    ],
+    link: "https://tick-tack-e-commers-hk65.vercel.app/",
+  },
+
+  // ,
+  // {
+  //     "name":"Software Village",
+  //     "description":"   This project was developed for Software Village to help students learn Frontend, Backend, Mobile Development, and Design. The course provides hands-on experience with modern technologies, real-world projects, and interactive lessons, preparing students for the industry.",
+  //     "image":image1,
+  //     "tecnolgy":["Html","Css","React","React-router-dom"],
+  //     "link":"https://ilklayihe.netlify.app/"
+  // },
+  // {
+  //     "name":"Rock-Paper-Scissors!",
+  //     "description":"This project is a digital version of the classic Rock-Paper-Scissors game, designed to provide a fun and interactive experience for users. It allows players to challenge the computer in a game of strategy and luck, with instant results and a score tracker to keep the competition exciting",
+  //     "image":image2,
+  //     "tecnolgy":["Html","Css","Javascript"],
+  //     "link":"https://homwork-game.netlify.app/"
+  // },
+];
